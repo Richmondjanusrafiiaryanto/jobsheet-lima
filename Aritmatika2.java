f396e214dfca4d5272c3d3aec986381781eb19e1
richmond janus rafii aryanto <53498537+Richmondjanusrafiiaryanto@users.noreply.github.com>
2019-08-22 15:20:54 +0700

    Add files via upload

---
+++ Aritmatika2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmatika.pkg2;
+
+/**
+ *
+ * @author surga 2019
+ */
+public class Aritmatika2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        double z = 2.5;
+        
+        int a = 0;
+        int b = 0;
+        
+        //Tambahkan sintaks operasi matematika di baris ini
+        a = x / y ;
+        b = (int) (x / z) ;
+        
+        System.out.println(a);
+        
+    }
+    
+}
---
+++ LatihanSoal8.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.soal.pkg8;
+
+/**
+ *
+ * @author surga 2019
+ */
+public class LatihanSoal8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x= 12;
+        int y= 11;
+        
+        y= ++x;
+        System.out.println("Nilai y sekarang:"+y); 
+        System.out.println("hasil x sekarang:"+x);
+    }
+    
+}
